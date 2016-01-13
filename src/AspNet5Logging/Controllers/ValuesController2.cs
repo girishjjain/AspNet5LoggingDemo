7f8a27bb98099faba0fcbcb8101507c13ac2073d
Girish Jayantilal Jain <girishjjain@hotmail.com>
2016-01-12 21:34:55 -0500

    AspNet 5 Logging Demo Source Code

---
+++ src/AspNet5Logging/Controllers/ValuesController2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet5Logging.Controllers
+{
+    [Route("api/values2")]
+    public class ValuesController2 : Controller
+    {
+        private readonly ILogger logger;
+
+        public ValuesController2(ILoggerFactory loggerFactory)
+        {
+            this.logger = loggerFactory.CreateLogger("AspNet5LoggingService");
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogInformation("Processing GET request for values.");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/AspNet5Logging/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AspNet5Logging
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            var listener = new TextWriterTraceListener("AspNet5LoggingService.txt");
+            listener.Filter = new SourceFilter("AspNet5LoggingService");
+
+            loggerFactory.AddTraceSource(
+                new SourceSwitch("AspNet5LoggingSwitch", "Verbose"),
+                listener
+            );
+
+            app.UseIISPlatformHandler();
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
